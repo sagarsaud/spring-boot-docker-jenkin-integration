cd4248e3cc0888657aa237304bf351b981246296
Sagar Kumar Saud <sagarsaud@gmail.com>
2023-05-04 22:14:54 +0545

    first commit

---
+++ src/main/java/com/sagar/docker/api/SpringBootDockerJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/docker/api/SpringBootDockerJenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
